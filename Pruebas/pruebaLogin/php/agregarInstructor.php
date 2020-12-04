7656a8d386ca1df0529336a45ab42272c07950b4
Mayri Bautista <16041328@itdurango.edu.mx>
2020-12-03 22:38:08 -0600

    ajustes en agregar Instructor

--- Pruebas/pruebaLogin/php/agregarInstructor.php
+++ Pruebas/pruebaLogin/php/agregarInstructor.php
@@ -11,16 +11,59 @@ setlocale(LC_TIME,'es_MX');
 
 $fechaNacimiento = strftime ('%Y-%m-%d', strtotime($inst->fechaNacimiento));
 
+if ($inst->horas == 1) {
+    $horas = "Sin horas";
+} elseif ($inst->horas == 2) {
+    $horas = "Tiempo Completo";
+} elseif ($inst->horas == 3) {
+    $horas = "Medio Tiempo";
+} elseif ($inst->horas == 4) {
+    $horas = "Tres Cuartos de Tiempo";
+} else {
+    $horas = "Asignaturas";
+}
+
+if ($inst->nivel == 1) {
+    $nivel = "Licenciatura";
+} elseif ($inst->nivel == 2) {
+    $nivel = "Maestría";
+} elseif ($inst->nivel == 3) {
+    $nivel = "Doctorado";
+} else {
+    $nivel = "Especialización";
+} 
+
+if ($inst->perfilDeseable == 1) {
+    $perfilDeseable = "Maestría";
+} elseif ($inst->perfilDeseable == 2) {
+    $perfilDeseable = "Doctorado";
+} else {
+    $perfilDeseable = "Especialización";
+} 
+
+if ($inst->funcionAdministrativa == 1) {
+    $funcionAdministrativa = "SI";
+} else {
+    $funcionAdministrativa = "NO";
+} 
+
 /* Query de insercion */
 $sql = "INSERT INTO usuario
         VALUES ('',
             '$inst->departamento',
             4,
+            '$inst->nombreUsuario',
+            '$inst->contrasena',
             '$inst->apellidoPaterno',
             '$inst->apellidoMaterno',
             '$inst->nombre',
-            '$inst->nombreUsuario',
-            '$inst->contrasena'
+            upper('$inst->RFC'),
+            upper('$inst->CURP'),
+            '$inst->horas',
+            '$inst->nivel',
+            '$inst->perfilDeseable',
+            'SI',
+            '$inst->funcionAdministrativa'
             )
         ";
 
